e5414bd19861e2283cc23817f20312570e64c485
marcostercio <marcostercioramos@gmail.com>
2020-12-15 23:55:57 -0300

    conexao
    alteração na conexao

--- modelo/DAO/conexao.php
+++ modelo/DAO/conexao.php
@@ -20,7 +20,7 @@ abstract class conexao {
     public static function getInstance() {
         //conexao com banco de dados
         try {
-            $pdo = new PDO("mysql:host=localhost; dbname=projetoCPA;", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+            $pdo = new PDO("mysql:host=us-cdbr-east-02.cleardb.com; dbname=heroku_cd18acf3ec923c1;", "b14d01e63a49ea", "3adcc74a", array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $pdo;
             } catch (PDOException $erro) {
